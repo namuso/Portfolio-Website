60931c73bedfc02ada9643719ac5cfd75a9fd032
Nick Amuso <amuson1@tcnj.edu>
2016-09-14 18:59:10 -0400

    Initial Commit

---
+++ js/navbarfix.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+  var $header = $("header");
+  var window_offset = 100 - $(window).scrollTop();
+  var my_id = 'mynavbar';
+
+  if (window_offset < 0 && document.getElementById(my_id).className.match(/(?:^|\s)not--fixed(?!\S)/)){
+      console.log('is--fixed');
+      document.getElementById(my_id).className = "is--fixed";
+  }
+  else if (window_offset >= 0 && document.getElementById(my_id).className.match(/(?:^|\s)is--fixed(?!\S)/)) {
+      console.log('not--fixed');
+      document.getElementById(my_id).className = "not--fixed";
+  }
+});
